16a527309a28418eef9420265e9d179e25d345d6
Park-Jimin-KAIST <parkjimin@kaist.ac.kr>
2025-07-12 15:50:24 +0900

    feat: NewPost 생성

---
+++ src/assets/images/NewPost_svg/index.js
@@ -0,0 +1,86 @@
+// This file is auto-generated.
+import { ReactComponent as Icon100mText } from './100m_text.svg';
+import { ReactComponent as Icon10mText } from './10m_text.svg';
+import { ReactComponent as Icon50mText } from './50m_text.svg';
+import { ReactComponent as IconLeeYeonJae } from './이연재.svg';
+import { ReactComponent as IconPintName } from './핀트 이름을 입력하세요.svg';
+import { ReactComponent as IconCaptionBox } from './caption_box.svg';
+import { ReactComponent as IconCaptionInstruction } from './caption_instruction.svg';
+import { ReactComponent as IconCaptionTitle } from './caption_title.svg';
+import { ReactComponent as IconImg8299 } from './IMG_8299.png.svg';
+import { ReactComponent as IconLocationAddrBox } from './location_addr_box.svg';
+import { ReactComponent as IconLocationHintBox } from './location_hint_box.svg';
+import { ReactComponent as IconLocationHintText } from './location_hint_text.svg';
+import { ReactComponent as IconLocationText } from './location_text.svg';
+import { ReactComponent as IconLocationTitle } from './location_title.svg';
+import { ReactComponent as IconPublicEntireBox } from './public_entire_box.svg';
+import { ReactComponent as IconPublicPrivateText } from './public_private_text.svg';
+import { ReactComponent as IconPublicPublicText } from './public_public_text.svg';
+import { ReactComponent as IconPublicSelectedBox } from './public_selected_box.svg';
+import { ReactComponent as IconPublicTitle } from './public_title.svg';
+import { ReactComponent as IconRectangle240652323 } from './Rectangle 240652323.svg';
+import { ReactComponent as IconRectangle240652340 } from './Rectangle 240652340.svg';
+import { ReactComponent as IconRectangle240652343 } from './Rectangle 240652343.svg';
+import { ReactComponent as IconTagEntireBox } from './tag_entire_box.svg';
+import { ReactComponent as IconTagFriendBox } from './tag_friend_box.svg';
+import { ReactComponent as IconTagFriendId } from './tag_friend_id.svg';
+import { ReactComponent as IconTagFriendName } from './tag_friend_name.svg';
+import { ReactComponent as IconTagPrivateText } from './tag_private_text.svg';
+import { ReactComponent as IconTagSearchIcon } from './tag_search_icon.svg';
+import { ReactComponent as IconTagSelectedBox } from './tag_selected_box.svg';
+import { ReactComponent as IconTagTitle } from './tag_title.svg';
+import { ReactComponent as IconUnlockEntireBox } from './unlock_entire_box.svg';
+import { ReactComponent as IconUnlockSelectedBox } from './unlock_selected_box.svg';
+import { ReactComponent as IconUnlockTitle } from './unlock_title.svg';
+import { ReactComponent as IconUploadBox } from './upload_box.svg';
+import { ReactComponent as IconUploadFileNameBox } from './upload_file_name_box.svg';
+import { ReactComponent as IconUploadMax } from './upload_max.svg';
+import { ReactComponent as IconUploadPlusIcon } from './upload_plus_icon.svg';
+import { ReactComponent as IconUploadTitle } from './upload_title.svg';
+import { ReactComponent as IconVector13892 } from './Vector 13892.svg';
+import { ReactComponent as IconVector13893 } from './Vector 13893.svg';
+import { ReactComponent as IconVid1349 } from './VID_1349.mp4.svg';
+
+export {
+  Icon100mText,
+  Icon10mText,
+  Icon50mText,
+  IconLeeYeonJae,
+  IconPintName,
+  IconCaptionBox,
+  IconCaptionInstruction,
+  IconCaptionTitle,
+  IconImg8299,
+  IconLocationAddrBox,
+  IconLocationHintBox,
+  IconLocationHintText,
+  IconLocationText,
+  IconLocationTitle,
+  IconPublicEntireBox,
+  IconPublicPrivateText,
+  IconPublicPublicText,
+  IconPublicSelectedBox,
+  IconPublicTitle,
+  IconRectangle240652323,
+  IconRectangle240652340,
+  IconRectangle240652343,
+  IconTagEntireBox,
+  IconTagFriendBox,
+  IconTagFriendId,
+  IconTagFriendName,
+  IconTagPrivateText,
+  IconTagSearchIcon,
+  IconTagSelectedBox,
+  IconTagTitle,
+  IconUnlockEntireBox,
+  IconUnlockSelectedBox,
+  IconUnlockTitle,
+  IconUploadBox,
+  IconUploadFileNameBox,
+  IconUploadMax,
+  IconUploadPlusIcon,
+  IconUploadTitle,
+  IconVector13892,
+  IconVector13893,
+  IconVid1349,
+};
