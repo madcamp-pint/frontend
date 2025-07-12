7bd7b01cee50a497c236646a1cc393c884bd750c
Yeonjae Lee <lyeonj@sookmyung.ac.kr>
2025-07-12 15:21:32 +0900

    fix: 파일명 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
-import Mainpage from './pages/Mainpage';
+import MainPage from './pages/MainPage';
 import PlacePintPage from './pages/PlacePintPage';
 import TimePintPage from './pages/TimePintPage';
 import MyPage from './pages/MyPage';
@@ -11,7 +11,7 @@ function App() {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
-      <Route path="/" element={<Mainpage />} />
+      <Route path="/" element={<MainPage />} />
       <Route path="/place" element={<PlacePintPage />} />
       <Route path="/time" element={<TimePintPage />} />
       <Route path="/mypage" element={<MyPage />} />
