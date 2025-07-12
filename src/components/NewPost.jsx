1d5a8b649e5309365223cbdb21e2a712a40317d8
Park-Jimin-KAIST <parkjimin@kaist.ac.kr>
2025-07-12 17:22:04 +0900

    fix: png로 변경

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,19 +1,15 @@
 import React from 'react';
-
-import { ReactComponent as EntireBox } from '../assets/images/NewPost_svg/entire_box.svg';
-import { ReactComponent as IconUploadPlusIcon } from '../assets/images/NewPost_svg/upload_plus_icon.svg';
-
+import EntireBox from '../assets/images/NewPost_png/entire_box.png';
+import IconUploadPlusIcon from '../assets/images/NewPost_png/upload_plus_icon.png';
 
 const NewPost = () => {
   return (
     <div style={{ position: 'relative', overflow: 'hidden', backgroundColor: 'lightgray', border: '2px solid blue' }}>
-      <EntireBox style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', zIndex: -1 }} />
-      <IconUploadPlusIcon style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', border: '2px solid red' }} />
+      <img src={EntireBox} style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', zIndex: -1 }} alt="Entire Box" />
+      <img src={IconUploadPlusIcon} style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', border: '2px solid red' }} alt="Upload Plus Icon" />
       {/* NewPostPopup 컴포넌트 */}
     </div>
   );
 };
 
 export default NewPost;
-
-
