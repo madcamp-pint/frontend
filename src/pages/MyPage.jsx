a441c0dc5b37dbc58de9f29c8805f93af6fc92ef
Yeonjae Lee <lyeonj@sookmyung.ac.kr>
2025-07-14 15:12:54 +0900

    feat: 전체 페이지에 Sidebar 적용

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -1,11 +1,33 @@
 import React from 'react'
 import styled from 'styled-components';
+import Sidebar from '../components/Sidebar';
+
+const Wrapper = styled.div`
+  display: flex;
+  width: 100vw;
+  height: 100vh;
+  background-color: #121212;
+`;
+
+const Container = styled.div`
+  flex: 1;
+  margin: 20px;
+  padding: 40px;
+  border-radius: 20px;
+  display: flex;
+  flex-direction: column;
+  background-color: #FAFAFA;
+  overflow-y: auto;
+`;
 
 const MyPage = () => {
   return (
-    <div>
-      <p>MyPage</p>
-    </div>
+    <Wrapper>
+      <Sidebar />
+      <Container>
+        <p>MyPage</p>
+      </Container>
+    </Wrapper>
   );
 }
 
